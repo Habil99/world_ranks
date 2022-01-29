b451454e4d6e3888198a1c16459cf2450060a91e
Habil99 <abiyevhabil3@gmail.com>
2022-01-29 19:03:34 +0400

    Fix all issues and Update node_modules

--- src/pages/country/[id].js
+++ src/pages/country/[id].js
@@ -1,97 +1,147 @@
 import { useEffect, useState } from "react";
 import Layout from "../../components/Layout/Layout";
-import styles from './country.module.css'
+import styles from "./country.module.css";
 
 const getCountry = async (id) => {
-    const response = await fetch(`http://restcountries.eu/rest/v2/alpha/${id}`)
-    const country = await response.json();
+  const response = await fetch(`https://restcountries.com/v2/name/${id}`);
+  const country = await response.json();
 
-    return country;
-}
+  return country;
+};
 
-const Country = ({ country }) => {
-    const [borders, setBorders] = useState([]);
-    const getBorders = async () => {
-        const borders = await Promise.all(
-            country.borders.map(border => getCountry(border))
+const getCountryByAplha3 = async (aplha3) => {
+  const response = await fetch(
+    `https://restcountries.com/v3.1/alpha/${aplha3}`
+  );
+  const country = await response.json();
+
+  return country;
+};
+
+const Country = ({ countries }) => {
+  const [borders, setBorders] = useState([]);
+  const getBorders = async () => {
+    const borders = countries[0].borders
+      ? await Promise.all(
+          countries[0].borders.map((border) => getCountryByAplha3(border))
         )
-        setBorders(borders);
-    };
+      : [];
+    setBorders(borders);
+  };
 
-    useEffect(() => {
-        getBorders();
-    }, [])
+  useEffect(() => {
+    getBorders();
+  }, []);
 
-    return (
-        <Layout title={country.name}>
-            <div className={styles.container}>
-                <div className={styles.container_left}>
-                    <div className={styles.overview_panel}>
-                        <img src={country.flag} alt={country.name} />
-                        <h1 className={styles.overview_name}>{country.name}</h1>
-                        <div className={styles.overview_region}>{country.region}</div>
+  return (
+    <Layout title={countries[0]?.name?.offical}>
+      {countries.map((country) => (
+        <div className={styles.container} key={country.cca3}>
+          <div className={styles.container_left}>
+            <div className={styles.overview_panel}>
+              <img src={country.flags.png} alt={country?.name?.offical} />
+              <h1 className={styles.overview_name}>{country?.name?.offical}</h1>
+              <div className={styles.overview_region}>{country.region}</div>
 
-                        <div className={styles.overview_numbers}>
-                            <div className={styles.overview_population}>
-                                <div className={styles.overview_value}>{country.population}</div>
-                                <div className={styles.overview_label}> Population</div>
-                            </div>
-                            <div className={styles.overview_area}>
-                                <div className={styles.overview_value}>{country.area}</div>
-                                <div className={styles.overview_label}>Area</div>
-                            </div>
-                        </div>
-                    </div>
+              <div className={styles.overview_numbers}>
+                <div className={styles.overview_population}>
+                  <div className={styles.overview_value}>
+                    {country.population}
+                  </div>
+                  <div className={styles.overview_label}> Population</div>
                 </div>
-                <div className={styles.container_right}>
-                    <div className={styles.details_panel}>
-                        <h4 className={styles.details_header}>Details</h4>
-                        <div className={styles.details_panel_row}>
-                            <div className={styles.details_panel_label}>Capital</div>
-                            <div className={styles.details_panel_value}>{country.capital}</div>
-                        </div>
-                        <div className={styles.details_panel_row}>
-                            <div className={styles.details_panel_label}>Languages</div>
-                            <div className={styles.details_panel_value}>{country.languages.map(({ name }) => name).join(", ")}</div>
-                        </div>
-                        <div className={styles.details_panel_row}>
-                            <div className={styles.details_panel_label}>Currencies</div>
-                            <div className={styles.details_panel_value}>{country.currencies.map(({ name }) => name).join(", ")}</div>
-                        </div>
-                        <div className={styles.details_panel_row}>
-                            <div className={styles.details_panel_label}>Native name</div>
-                            <div className={styles.details_panel_value}>{country.nativeName}</div>
-                        </div>
-                        <div className={styles.details_panel_row}>
-                            <div className={styles.details_panel_label}>Gini</div>
-                            <div className={styles.details_panel_value}>{country.gini ? country.gini : "0"}%</div>
-                        </div>
-                        <div className={styles.details_panel_borders}>
-                            <div className={styles.details_panel__borders_label}>Neighbouring Countries</div>
-                            <div className={styles.details_panel_borders_container}>
-                                {borders.length > 0 ? borders.map(({ flag, name }) => (
-                                    <div className={styles.details_panel_borders_country} key={flag}>
-                                        <img src={flag} alt={name} />
-                                        <div className={styles.details_panel_borders_name}>{name}</div>
-                                    </div>
-                                )) : <p className={styles.details_panel__borders_notFound}>There is not neighboring country</p>}
-                            </div>
-                        </div>
-                    </div>
+                <div className={styles.overview_area}>
+                  <div className={styles.overview_value}>{country.area}</div>
+                  <div className={styles.overview_label}>Area</div>
                 </div>
+              </div>
             </div>
-        </Layout>
-    )
-}
+          </div>
+          <div className={styles.container_right}>
+            <div className={styles.details_panel}>
+              <h4 className={styles.details_header}>Details</h4>
+              <div className={styles.details_panel_row}>
+                <div className={styles.details_panel_label}>Capital</div>
+                <div className={styles.details_panel_value}>
+                  {country?.capital?.map((capital) => capital).join(",")}
+                </div>
+              </div>
+              <div className={styles.details_panel_row}>
+                <div className={styles.details_panel_label}>Languages</div>
+                <div className={styles.details_panel_value}>
+                  {Object.values(country.languages)
+                    .map((language) => language)
+                    .join(",")}
+                </div>
+              </div>
+              <div className={styles.details_panel_row}>
+                <div className={styles.details_panel_label}>Currencies</div>
+                <div className={styles.details_panel_value}>
+                  {Object.values(country.currencies)
+                    .map((currency) => currency.name)
+                    .join(",")}
+                </div>
+              </div>
+              <div className={styles.details_panel_row}>
+                <div className={styles.details_panel_label}>Native name</div>
+                <div className={styles.details_panel_value}>
+                  {country?.name?.offical}
+                </div>
+              </div>
+              <div className={styles.details_panel_row}>
+                <div className={styles.details_panel_label}>Gini</div>
+                <div className={styles.details_panel_value}>
+                  {country.gini
+                    ? Object.values(country.gini)
+                        .map((gini) => gini)
+                        .join(",") || "0"
+                    : "0"}
+                  %
+                </div>
+              </div>
+              <div className={styles.details_panel_borders}>
+                <div className={styles.details_panel__borders_label}>
+                  Neighbouring Countries
+                </div>
+                <div className={styles.details_panel_borders_container}>
+                  {borders.length > 0 ? (
+                    borders.map((border) => (
+                      <div
+                        className={styles.details_panel_borders_country}
+                        key={border[0].name.official}
+                      >
+                        <img
+                          src={border[0].flags.png}
+                          alt={border[0].name.official}
+                        />
+                        <div className={styles.details_panel_borders_name}>
+                          {border[0].name.official}
+                        </div>
+                      </div>
+                    ))
+                  ) : (
+                    <p className={styles.details_panel__borders_notFound}>
+                      There is not neighboring country
+                    </p>
+                  )}
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      ))}
+    </Layout>
+  );
+};
 
 export default Country;
 
 export const getServerSideProps = async ({ params }) => {
-    const country = await getCountry(params.id);
+  const countries = await getCountryByAplha3(params.id);
 
-    return {
-        props: {
-            country
-        }
-    }
-}
+  return {
+    props: {
+      countries,
+    },
+  };
+};
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,40 +1,45 @@
 import styles from "../styles/Home.module.css";
-import Layout from '../components/Layout/Layout'
-import SearchInput from '../components/SearchInput/SearhcInput'
+import Layout from "../components/Layout/Layout";
+import SearchInput from "../components/SearchInput/SearhcInput";
 import CountriesTable from "../components/CountriesTable/CountriesTable";
 import { useState } from "react";
 
 export default function Home({ countries }) {
-    const [keyword, setKeyword] = useState("");
-    const filterCountries = countries.filter(country => country.name.toLowerCase().includes(keyword)
-        || country.region.toLowerCase().includes(keyword)
-        || country.subregion.toLowerCase().includes(keyword)
-    )
+  const [keyword, setKeyword] = useState("");
+  const filterCountries = countries.filter(
+    (country) =>
+      country.name.official.toLowerCase().includes(keyword) ||
+      country.region.toLowerCase().includes(keyword) ||
+      country.subregion.toLowerCase().includes(keyword)
+  );
 
-    const onInputChange = (e) => {
-        e.preventDefault();
-        setKeyword(e.target.value.toLowerCase());
-    }
-    return (
-        <Layout>
-            <div className={styles.inputContainer}>
-                <div className={styles.counts}>Found {countries.length} countries</div>
-                <div className={styles.input}>
-                    <SearchInput onChange={onInputChange} placeholder="Filter by Name, Region or Subregion" />
-                </div>
-            </div>
-            <CountriesTable countries={filterCountries} />
-        </Layout>
-    )
+  const onInputChange = (e) => {
+    e.preventDefault();
+    setKeyword(e.target.value.toLowerCase());
+  };
+  return (
+    <Layout>
+      <div className={styles.inputContainer}>
+        {/* <div className={styles.counts}>Found {countries.length} countries</div> */}
+        <div className={styles.input}>
+          <SearchInput
+            onChange={onInputChange}
+            placeholder="Filter by Name, Region or Subregion"
+          />
+        </div>
+      </div>
+      <CountriesTable countries={filterCountries} />
+    </Layout>
+  );
 }
 
 export const getStaticProps = async () => {
-    const response = await fetch("https://restcountries.eu/rest/v2/all");
-    const countries = await response.json();
+  const response = await fetch("https://restcountries.com/v3.1/region/europe");
+  const countries = await response.json();
 
-    return {
-        props: {
-            countries
-        }
-    }
-}
+  return {
+    props: {
+      countries,
+    },
+  };
+};
